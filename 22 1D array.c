4a33f020c18272ad37b8da6cd976ba912ee29123
nithish200722-boop <nithish200722@gmail.com>
2026-03-04 09:11:09 +0530

    Add files via upload

---
+++ 22 1D array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    int Count=0;
+    for(int i=1;i<n-1;i++)
+    {
+        if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
+        {
+            Count++;
+        }
+    }
+    printf("%d",Count);
+    return 0;
+}
